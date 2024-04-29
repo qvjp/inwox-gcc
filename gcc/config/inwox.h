8c4b508975d91e105c2e2bc03ab65ead6cedfe2f
Qu Junping <qvjunping@gmail.com>
2024-04-29 23:34:12 +0800

    Add inwox target

---
+++ gcc/config/inwox.h
@@ -0,0 +1,21 @@
+#undef TARGET_INWOX
+#define TARGET_INWOX 1
+
+#undef LIB_SPEC
+#define LIB_SPEC "-lc"
+
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+
+#undef TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS() \
+  do { \
+    builtin_define ("__inwox__"); \
+    builtin_define ("__unix__"); \
+    builtin_assert ("system=inwox"); \
+    builtin_assert ("system=unix"); \
+    builtin_assert ("system=posix"); \
+  } while(0);
